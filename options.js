7a76d0ef9920422873634ddd5c6add5ef34fd153
Bill Xiong <billxiong99@gmail.com>
2015-08-13 07:33:53 -0700

    Added options to set the other affiliate codes

--- options.js
+++ options.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('code').value = localStorage['affiliate_code'] || '';
+	document.getElementById('amzncode').value = localStorage['amzn_code'] || '';
+  document.getElementById('ncixcode').value = localStorage['ncix_code'] || '';
+  document.getElementById('neweggcode').value = localStorage['newegg_code'] || '';
 
 	document.getElementById('save').addEventListener('click', function() {
-		localStorage['affiliate_code'] = document.getElementById('code').value;
+		localStorage['amzn_code'] = document.getElementById('amzncode').value;
+    localStorage['ncix_code'] = document.getElementById('ncixcode').value;
+    localStorage['newegg_code'] = document.getElementById('neweggcode').value;
 
 		var status = document.getElementById('status');
 		status.innerText = 'Options Saved.';
